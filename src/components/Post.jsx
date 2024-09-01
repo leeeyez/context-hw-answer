1b6fd61aefaf60ef0da8b0cdd475937ce267a5c5
solux <lee.yeji0327@gmail.com>
2024-09-01 12:34:03 +0900

    코드 설명 추가

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,7 +14,7 @@ const Post = ({ name, content }) => {
   const { tagName } = useTag(); // 선택된 태그의 이름을 context로 부터 가져옴
 
   // 선택된 태그 이름이 존재하지 않거나(아직 태그 선택 안 한 경우),
-  // prop으로 내려받은 posts의 name과 선택된 태그 이름이 같은 경우만 렌더링(선택한 태그만 렌더링)
+  // prop으로 내려받은 posts의 name과 선택된 태그 이름이 일치하는 경우만 렌더링(선택한 태그만 렌더링)
   if (!tagName || tagName === name) {
     return (
       <Box>
